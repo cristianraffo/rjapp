2e1ca86719759466e5e27682ba8d2ad51c0295ba
CristianR <cristiangraffo33@gmail.com>
2022-02-28 18:55:44 -0300

    fix 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,20 @@
+import "./NotFoundPage.css";
+
 const NotFoundPage = () => {
-    return(
-    <h1>No anda</h1>
-    )
-}
+  return (
+    <section class="error text-center d-flex">
+    <div className="container m-auto">
+      <img className="img-fluid" src="404-logo.png" alt="404 image" />
+      <h2 className="error-title">
+        <a className="error-link" href="./">
+          Beam me up
+        </a>
+        , Scotty!
+      </h2>
+      <p className="error-subtitle">There is no intelligent life down here.</p>
+    </div>
+    </section>
+  );
+};
 
 export default NotFoundPage;
